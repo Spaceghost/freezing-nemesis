3a824d40468934ff15e34ddd31c212edc571334f
Johnneylee Jack Rollins <Johnneylee.Rollins@gmail.com>
2012-09-23 17:19:18 -0400

    New rails repo 3.2.8
    Base install using rails 3.2.8
    
    * Used the skip test unit switch
    * Used the skip activerecord switch
    
    Signed-off-by: Johnneylee Jack Rollins <Johnneylee.Rollins@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreezingNemesis::Application.config.secret_token = '0f59122201b0a79c41fd5bd337b675fcf6c66dea1ca5cc30f64b926cad8c559c24bdc451fc60c43f2323fded05cbc8dde564991c7a0f28d53eb1ae6899e4044f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FreezingNemesis::Application.config.session_store :cookie_store, key: '_Freezing-Nemesis_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FreezingNemesis::Application.config.session_store :active_record_store
